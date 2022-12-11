90f2ed8fb6d2d5d71ef7eb17a060c5c98a58bb2b
Mostafa <mostafa152804@gmail.com>
2022-12-11 06:37:43 +0100

    updated CORS Policy

--- src/main/java/com/example/mostafa/ecommercebackend/dao/ProductRepository.java
+++ src/main/java/com/example/mostafa/ecommercebackend/dao/ProductRepository.java
@@ -4,7 +4,8 @@ import com.example.mostafa.ecommercebackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+
+@CrossOrigin("http://mostaxd-ecommerce.s3-website.eu-central-1.amazonaws.com")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
